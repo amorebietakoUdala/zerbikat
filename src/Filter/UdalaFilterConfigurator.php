9e29ad6d7f80e345aa7dd09a512e35f03aea6ce6
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-12-23 10:57:24 +0100

    Changed Configurator filter to EventSubscriber

--- src/Filter/UdalaFilterConfigurator.php
+++ src/Filter/UdalaFilterConfigurator.php
@@ -4,32 +4,33 @@ namespace App\Filter;
 
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Annotations\Reader;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
-class Configurator
+class UdalaFilterConfigurator implements EventSubscriberInterface
 {
     protected $em;
     protected $tokenStorage;
     protected $reader;
 
-    public function __construct(ObjectManager $em, TokenStorageInterface $tokenStorage, Reader $reader)
+    public function __construct(EntityManagerInterface $em, TokenStorageInterface $tokenStorage, Reader $reader)
     {
         $this->em              = $em;
         $this->tokenStorage    = $tokenStorage;
         $this->reader          = $reader;
     }
 
-    public function onKernelRequest()
+    public function onKernelRequest(RequestEvent $event)
     {
         if ($user = $this->getUser()) {
             $filter = $this->em->getFilters()->enable('udala_filter');
-            if ($user->getudala()) {
+            /** @var App\Entity\User $user */
+            if ($user->getUdala()) {
                 $filter->setParameter('udala_id', $user->getudala()->getId());
                 $filter->setAnnotationReader($this->reader);
-            } else
-            {
-                return '' ;
             }
         }
     }
@@ -49,5 +50,12 @@ class Configurator
         }
 
         return $user;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
     }
 }
